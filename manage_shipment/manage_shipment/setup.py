2bc86d518760c88a74727e2b7cca2ee579c3d522
Gaurang Thumar <126887545+Gaurang1979@users.noreply.github.com>
2026-09-16 21:06:27 +0530

    Ensure existing default couriers receive generic adapter configuration

--- manage_shipment/manage_shipment/setup.py
+++ manage_shipment/manage_shipment/setup.py
@@ -1,21 +1,14 @@
 import frappe
 
-DEFAULT_PROVIDERS = [
-    ("Trackon", "TRACKON"), ("Shree Maruti", "MARUTI"), ("DTDC", "DTDC"),
-    ("Anjani", "ANJANI"), ("Professional Couriers", "PROFESSIONAL"), ("Delhivery", "DELHIVERY"),
-    ("Blue Dart", "BLUEDART"), ("India Post / Speed Post", "INDIAPOST"), ("XpressBees", "XPRESSBEES"),
-    ("Ecom Express", "ECOMEXPRESS"), ("Gati", "GATI"), ("Safexpress", "SAFEXPRESS"),
-    ("Ekart", "EKART"), ("Shadowfax", "SHADOWFAX"),
-]
+DEFAULT_PROVIDERS = [("Trackon","TRACKON"),("Shree Maruti","MARUTI"),("DTDC","DTDC"),("Anjani","ANJANI"),("Professional Couriers","PROFESSIONAL"),("Delhivery","DELHIVERY"),("Blue Dart","BLUEDART"),("India Post / Speed Post","INDIAPOST"),("XpressBees","XPRESSBEES"),("Ecom Express","ECOMEXPRESS"),("Gati","GATI"),("Safexpress","SAFEXPRESS"),("Ekart","EKART"),("Shadowfax","SHADOWFAX")]
 GENERIC_ADAPTER = "manage_shipment.manage_shipment.integrations.generic.GenericHTTPAdapter"
 
 
 def after_install():
     for provider_name, provider_code in DEFAULT_PROVIDERS:
-        if not frappe.db.exists("Courier Service Provider", {"provider_name": provider_name}):
-            frappe.get_doc({
-                "doctype": "Courier Service Provider", "provider_name": provider_name,
-                "provider_code": provider_code, "enabled": 1, "integration_enabled": 0,
-                "adapter_path": GENERIC_ADAPTER,
-            }).insert(ignore_permissions=True)
+        name = frappe.db.get_value("Courier Service Provider", {"provider_name": provider_name}, "name")
+        if not name:
+            frappe.get_doc({"doctype":"Courier Service Provider","provider_name":provider_name,"provider_code":provider_code,"enabled":1,"integration_enabled":0,"adapter_path":GENERIC_ADAPTER}).insert(ignore_permissions=True)
+        else:
+            frappe.db.set_value("Courier Service Provider", name, {"provider_code":provider_code,"adapter_path":GENERIC_ADAPTER}, update_modified=False)
     frappe.db.commit()
