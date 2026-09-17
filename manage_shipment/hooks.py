2e1d282ed551ab8e9a4991a1eafef8088a23c74e
Gaurang Thumar <126887545+Gaurang1979@users.noreply.github.com>
2026-09-17 12:56:28 +0530

    fix: register tracking integration form script

--- manage_shipment/hooks.py
+++ manage_shipment/hooks.py
@@ -9,7 +9,6 @@ required_apps = ["erpnext"]
 after_install = "manage_shipment.manage_shipment.setup.after_install"
 add_to_apps_screen = [{"name": "manage_shipment", "title": "Manage Shipment", "route": "/app/shipment-dashboard"}]
 app_include_css = "/assets/manage_shipment/css/manage_shipment.css"
-doctypes_js = {}
 doctypes_js = {"Shipment": "public/js/shipment.js", "Tracking API Integration": "public/js/tracking_api_integration.js"}
 fixtures = [{"dt": "Custom Field", "filters": [["module", "=", "Manage Shipment"]]}]
 doc_events = {"Delivery Note": {"on_submit": "manage_shipment.manage_shipment.doctype_events.create_shipment_from_delivery_note"}}
