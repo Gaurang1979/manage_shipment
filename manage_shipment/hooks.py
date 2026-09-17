4f623bd873329483f9437002e14ab305b99ca3a1
Gaurang Thumar <126887545+Gaurang1979@users.noreply.github.com>
2026-09-17 12:56:23 +0530

    feat: load tracking integration form script

--- manage_shipment/hooks.py
+++ manage_shipment/hooks.py
@@ -9,7 +9,8 @@ required_apps = ["erpnext"]
 after_install = "manage_shipment.manage_shipment.setup.after_install"
 add_to_apps_screen = [{"name": "manage_shipment", "title": "Manage Shipment", "route": "/app/shipment-dashboard"}]
 app_include_css = "/assets/manage_shipment/css/manage_shipment.css"
-doctype_js = {"Shipment": "public/js/shipment.js"}
+doctypes_js = {}
+doctypes_js = {"Shipment": "public/js/shipment.js", "Tracking API Integration": "public/js/tracking_api_integration.js"}
 fixtures = [{"dt": "Custom Field", "filters": [["module", "=", "Manage Shipment"]]}]
 doc_events = {"Delivery Note": {"on_submit": "manage_shipment.manage_shipment.doctype_events.create_shipment_from_delivery_note"}}
 scheduler_events = {"cron": {"*/10 * * * *": ["manage_shipment.manage_shipment.tasks.track_shipments"], "5 * * * *": ["manage_shipment.manage_shipment.tasks.update_follow_up_flags"]}}
